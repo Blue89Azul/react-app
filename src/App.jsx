998839f02e814c95ca506c328487e0bfe89d8050
Blue89Azul <life.89.blue@gmail.com>
2024-03-25 13:26:22 +0900

    installed router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<HomePage />} />)
+);
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      <RouterProvider router={router} />
+      {/* <Navbar />
       <Hero title="Main Titile" subtitle="This is subtitle" />
       <HomeCards />
       <JobListings />
-      <ViewAllJobs />
+      <ViewAllJobs /> */}
     </>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import HomeCards from "../components/HomeCards";
+import JobListings from "../components/JobListings";
+import ViewAllJobs from "../components/ViewAllJobs";
+
+const HomePage = () => {
+    return(
+        <Hero />
+    );
+}
+
+export default HomePage
